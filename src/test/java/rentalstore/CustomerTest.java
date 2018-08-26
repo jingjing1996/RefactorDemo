4f94e6465a02ed9c6a6bd1b47640985cc169eb0f
jingjing1996 <1372031421@qq.com>
2018-08-26 17:52:49 +0800

    add one regular rental for 3 days test

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class CustomerTest {
 
     @Test
-    public void should_return_result_when_customer_given_and_one_rental_given_and_rent_day_is_one(){
+    public void should_return_result_when_customer_given_and_one_regular_rental_given_and_rent_day_is_one(){
         //Given
         Customer customer = new Customer("joy");
         Movie movie = new Movie("Flipped",Movie.REGULAR);
@@ -21,6 +21,25 @@ public class CustomerTest {
         Assert.assertEquals("Rental Record for joy\n" +
                 "\tFlipped\t2.0\n" +
                 "Amount owed is2.0\n" +
+                "You earned1 frequent renter points",result);
+    }
+
+    @Test
+    public void should_return_result_when_customer_given_and_one_regular_rental_given_and_rent_day_is_three(){
+        //Given
+        Customer customer = new Customer("joy");
+        Movie movie = new Movie("Flipped",Movie.REGULAR);
+        Rental rental1 = new Rental(movie,3);
+        customer.addRental(rental1);
+
+        //When
+        String result = customer.statement();
+
+
+        //Then
+        Assert.assertEquals("Rental Record for joy\n" +
+                "\tFlipped\t3.5\n" +
+                "Amount owed is3.5\n" +
                 "You earned1 frequent renter points",result);
     }
 
